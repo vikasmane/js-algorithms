61c94a2357c2bd156a4d1eacd1a7c9e0dcc96b07
vikasmane <vikas.bal.mane@gmail.com>
2018-10-28 23:44:10 +0530

    bubble sort added

---
+++ src/bubble.js
@@ -0,0 +1,18 @@
+// Bubble sort is in place sorting algorithm 
+// Bubble sort would sort list with worst case time complexity of (O(n^2)).
+// Bubble sort's space complexity is O(1).
+function bubbleSort(arr){
+    // iterate over an array to its length
+    for(let i=0; i<arr.length; i++){
+        // iterate over an array from 1 to length - i
+        for(let j=0; j<arr.length-i; j++){
+            if(arr[j]>arr[j+1]) swap(arr, j, j+1);
+        }
+    }
+    return arr;
+}
+function swap(arr, i, j){
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+}
+
+console.log(bubbleSort([56,65,26,5,6,32,8,97,23,36]));
