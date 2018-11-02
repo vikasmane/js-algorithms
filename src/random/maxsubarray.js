0fa596365492698ed4f558bbbc7d650d49ef6602
vikasmane <vikas.bal.mane@gmail.com>
2018-11-03 01:24:00 +0530

    maxsubarray added

---
+++ src/random/maxsubarray.js
@@ -0,0 +1,23 @@
+const expect = require('expect');
+function maxSubArray(arr){
+    let startInd = 0;
+    let endInd = arr.length - 1;
+    let currSum = -Infinity;
+    let maxSum = 0;
+    let currentStart = 0;
+    arr.forEach((el, i) => {
+        currSum += el;
+        if(currSum > maxSum){
+            maxSum = currSum;
+            startInd = currentStart;
+            endInd = i;
+        }
+
+        if(currSum < 0){
+            currSum = 0;
+            currentStart = i + 1;
+        }
+    });
+    return arr.slice(startInd, endInd+1);
+}
+expect(maxSubArray([15,-68,37,36,-75,69,55,-56])).toEqual([69,55]);
